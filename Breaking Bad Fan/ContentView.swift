815d3c611f1413ba77d507559bc284d2a54a08f4
Marc Aupont <mewsicmasta@gmail.com>
2021-04-18 19:08:45 -0400

    created temporary CardView to test out layout

--- Breaking Bad Fan/ContentView.swift
+++ Breaking Bad Fan/ContentView.swift
@@ -8,14 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ScrollView {
+            InfinitePageView(pages: [
+                CardView(color: .red){
+                    print("Red was pressed")
+                },
+                CardView(color: .green) {
+                    print("Green was pressed")
+                },
+                CardView(color: .gray) {
+                    print("Gray was presed")
+                }
+            ])
+            .frame(height: 300)
+            LazyVGrid(columns: [GridItem(.flexible(minimum: 100)), GridItem(.flexible(minimum: 100))], content: {
+                ForEach(0 ..< 62) { item in
+                    RoundedRectangle(cornerRadius: 20)
+                        .fill(Color.green)
+                        .frame(height: 100)
+                }.padding(.horizontal, 10)
+            })
+            
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct CardView: View {
+    var color: Color
+    var handler: () -> Void
+    var body: some View {
+        Button(action: {
+            handler()
+        }, label: {
+            RoundedRectangle(cornerRadius: 10)
+                .fill(color)
+                .padding(24)
+        })
     }
 }
