6f169114a36de250543419d9751e5f95b1ab18bb
Marc Aupont <mewsicmasta@gmail.com>
2021-04-18 17:16:53 -0400

    Initial Commit

---
+++ Breaking Bad Fan/Breaking_Bad_FanApp.swift
@@ -0,0 +1,17 @@
+//
+//  Breaking_Bad_FanApp.swift
+//  Breaking Bad Fan
+//
+//  Created by Marc Aupont on 4/18/21.
+//
+
+import SwiftUI
+
+@main
+struct Breaking_Bad_FanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
