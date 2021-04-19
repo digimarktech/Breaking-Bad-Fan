9bbd36fd6bba33a296c9196a1e4989c7729b092c
Marc Aupont <mewsicmasta@gmail.com>
2021-04-18 20:13:09 -0400

    experimental model and downloading of data

--- Breaking Bad Fan/ContentView.swift
+++ Breaking Bad Fan/ContentView.swift
@@ -9,10 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @StateObject var viewModel = ViewModel()
+    
+    let columns = Array(repeating: GridItem(.flexible(minimum: 100)), count: 2)
+    
     var body: some View {
         ScrollView {
             InfinitePageView(pages: [
-                CardView(color: .red){
+                CardView(color: .red) {
                     print("Red was pressed")
                 },
                 CardView(color: .green) {
@@ -23,14 +27,16 @@ struct ContentView: View {
                 }
             ])
             .frame(height: 300)
-            LazyVGrid(columns: [GridItem(.flexible(minimum: 100)), GridItem(.flexible(minimum: 100))], content: {
-                ForEach(0 ..< 62) { item in
+            LazyVGrid(columns: columns, content: {
+                ForEach(viewModel.characters) { character in
                     RoundedRectangle(cornerRadius: 20)
                         .fill(Color.green)
                         .frame(height: 100)
                 }.padding(.horizontal, 10)
             })
-            
+        }
+        .onAppear {
+            self.viewModel.getCharacters()
         }
     }
 }
@@ -52,5 +58,50 @@ struct CardView: View {
                 .fill(color)
                 .padding(24)
         })
+    }
+}
+
+final class ViewModel: ObservableObject {
+    @Published var characters = [ShowCharacter]()
+    
+    func getCharacters() {
+        URLSession.shared.dataTask(with: URL(string: "https://www.breakingbadapi.com/api/characters")!) { (data, _, error) in
+            if let error = error {
+                print(error.localizedDescription)
+                return
+            }
+            guard let data = data else {
+                return
+            }
+            if let characters = try? JSONDecoder().decode([ShowCharacter].self, from: data) {
+                DispatchQueue.main.async {
+                    self.characters = characters
+                }
+            }
+        }.resume()
+    }
+}
+
+struct ShowCharacter: Codable, Identifiable {
+    let id: Int
+    let name: String
+    let birthday: String
+    let occupation: [String]
+    let imageURL: URL
+    let status: String
+    let nickname: String
+    let seasons: [Int]
+    let realName: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id = "char_id"
+        case name
+        case birthday
+        case occupation
+        case imageURL = "img"
+        case status
+        case nickname
+        case seasons = "appearance"
+        case realName = "portrayed"
     }
 }
